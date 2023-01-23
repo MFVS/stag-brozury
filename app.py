c883b0aa0357608e782c016ba030e0d829642412
yoptohlejepeta <peta.kotlan@seznam.cz>
2023-01-23 10:09:26 +0100

    added static, basic structure

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from stag_data import *
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/<predmet>')
-def home(predmet):
+def overview(predmet):
     data = get_data(predmet)
-    return render_template('home.html', data=data)
+    return render_template('predmet.html', data=data)
+
+@app.route('/favicon.ico')
+def icon():
+    return send_from_directory('static','ujep_erasmus.png')
 
 
 if __name__ == "__main__":
--- stag_data.py
+++ stag_data.py
@@ -4,9 +4,3 @@ import pandas as pd
 def get_data(zkratka):
     df = pd.read_csv(f'https://ws.ujep.cz/ws/services/rest2/predmety/getPredmetInfo?zkratka={zkratka}&lang=en&outputFormat=CSV&katedra=KI&rok=2022', sep=';', encoding='cp1250')
     return df
-
-# pd.set_option('display.max_colwidth', None)
-
-# df = get_data('EAPR1')
-# print(df['anotace'])
-
