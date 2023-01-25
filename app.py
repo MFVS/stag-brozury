4d23c86d8f96113663ed7916a3c64107d4690f06
yoptohlejepeta <peta.kotlan@seznam.cz>
2023-01-25 23:20:28 +0100

    working on tables, added static

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, send_from_directory
 from stag_data import *
 
 app = Flask(__name__)
-
+    
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', katedry=katedry.keys())
 
-@app.route('/<predmet>')
-def overview(predmet):
-    data = get_data(predmet)
-    return render_template('predmet.html', data=data)
+@app.route('/<katedra>')
+def overview(katedra):
+    data = get_data(katedra)
+    return render_template('predmet.html', data=data, katedra=katedra)
 
 @app.route('/favicon.ico')
 def icon():
--- stag_data.py
+++ stag_data.py
@@ -1,6 +1,26 @@
 import pandas as pd
 
+katedry = {
+    'KI' : ['EAPR1','EAPR2','ECIS','EDAV', 'ECGR', 'RDBS',],
+    'KMA': ['E101', 'E103', 'E104', 'E105'],
+    'KGEO': [],
+    'KBI': [],
+    'KCH': [],
+    'KFY': ['E511']
+}
 
-def get_data(zkratka):
-    df = pd.read_csv(f'https://ws.ujep.cz/ws/services/rest2/predmety/getPredmetInfo?zkratka={zkratka}&lang=en&outputFormat=CSV&katedra=KI&rok=2022', sep=';', encoding='cp1250')
-    return df
+
+def get_data(katedra):
+    zkratky = katedry.get(katedra)
+    dfs = []
+    for i in zkratky:
+        dfs.append(pd.read_csv(f'https://ws.ujep.cz/ws/services/rest2/predmety/getPredmetInfo?zkratka={i}&lang=en&outputFormat=CSV&katedra={katedra}&rok=2022', 
+            sep=';', 
+            encoding='cp1250'))
+    return dfs
+
+# data = get_data('KI')
+# # for i in range(len(data)):
+# #     print(data[i]['zkratka'][0])
+    
+# print(data[0]['prehledLatky'][0])
