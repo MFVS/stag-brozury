ea37ca387cc0ea0f70ac42ef4b0554c3a5f938fd
yoptohlejepeta <peta.kotlan@seznam.cz>
2023-03-15 20:55:08 +0100

    formatted

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ from stag_data import *
 app = Flask(__name__)
 
 # Endpointy
-    
-@app.route('/')
-def home():
-    return render_template('home.html', katedry=katedry.keys())
 
-@app.route('/<katedra>')
+
+@app.route("/")
+def home():
+    return render_template("home.html", katedry=katedry.keys())
+
+
+@app.route("/<katedra>")
 def overview(katedra):
     data = get_data(katedra)
-    return render_template('predmet.html', data=data, katedra=katedra)
+    return render_template("predmet.html", data=data, katedra=katedra)
 
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def icon():
-    return send_from_directory('static','stag_favcon.ico')
+    return send_from_directory("static", "stag_favcon.ico")
 
 
 if __name__ == "__main__":
--- stag_data.py
+++ stag_data.py
@@ -3,12 +3,12 @@ import pandas as pd
 # TODO: Přeměty podle oborů.
 
 katedry = {
-    'KI' : ['EAPR1','EAPR2','ECIS','EDAV', 'ECGR', 'NSQL','RDBS', 'EIS'],
-    'KMA': ['E101', 'E103', 'E104', 'E105'],
-    'KGEO': [],
-    'KBI': [],
-    'KCH': [],
-    'KFY': ['E511']
+    "KI": ["EAPR1", "EAPR2", "ECIS", "EDAV", "ECGR", "NSQL", "RDBS", "EIS"],
+    "KMA": ["E101", "E103", "E104", "E105"],
+    "KGEO": [],
+    "KBI": [],
+    "KCH": [],
+    "KFY": ["E511"],
 }
 
 
@@ -16,8 +16,11 @@ def get_data(katedra):
     zkratky = katedry.get(katedra)
     dfs = []
     for i in zkratky:
-        dfs.append(pd.read_csv(f'https://ws.ujep.cz/ws/services/rest2/predmety/getPredmetInfo?zkratka={i}&lang=en&outputFormat=CSV&katedra={katedra}&rok=2022', 
-            sep=';', 
-            encoding='cp1250'))
+        dfs.append(
+            pd.read_csv(
+                f"https://ws.ujep.cz/ws/services/rest2/predmety/getPredmetInfo?zkratka={i}&lang=en&outputFormat=CSV&katedra={katedra}&rok=2022",
+                sep=";",
+                encoding="cp1250",
+            )
+        )
     return dfs
-
